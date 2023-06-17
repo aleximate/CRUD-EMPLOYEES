219f50f27e2e33acdb3d1b47fb24cd4b0934fbaa
Alexander <alshmex209@gmail.com>
2023-06-17 12:48:17 -0500

    feat: add HTTP methos on Controllers for employees

--- demo/src/main/java/com/example/demo/Controller/EmployeeController.java
+++ demo/src/main/java/com/example/demo/Controller/EmployeeController.java
@@ -1,21 +1,61 @@
 package com.example.demo.Controller;
 
+import com.example.demo.Exceptions.ExceptionNotFound;
 import com.example.demo.Repositories.EmployeeRepositorie;
 import com.example.demo.models.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
-@RequestMapping("/api/")
+@RequestMapping("/api/v1/")
 public class EmployeeController {
     @Autowired
     private EmployeeRepositorie employeeRepositorie;
-    @GetMapping("/empleados")
+    @GetMapping("/employees")
     public List<Employee> ListEmployees(){
         return employeeRepositorie.findAll();
+    }
+
+    @PostMapping("/employees")
+    @ResponseBody
+    public Employee saveEmployee(@RequestBody Employee employee){
+        return employeeRepositorie.save(employee);
+    }
+    @GetMapping("/employees/{id}")
+    public ResponseEntity<Employee>getEmployeeById(@PathVariable Long id){
+        Employee employee= employeeRepositorie.findById(id).orElseThrow(()->new RuntimeException("Doesn't exists this ID: "+id));
+
+        return ResponseEntity.ok(employee);
+    }
+
+    @PutMapping("/employees/{id}")
+    @ResponseBody
+    public ResponseEntity<Employee>updateEmployee(@PathVariable Long id,@RequestBody Employee employee){
+        Employee employee1= employeeRepositorie.findById(id).orElseThrow(()->new RuntimeException("Doesn't exists this ID: "+id));
+
+        employee1.setName(employee.getName());
+        employee1.setLastname(employee.getLastname());
+        employee1.setEmail(employee.getEmail());
+
+        Employee updatedEmployee=employeeRepositorie.save(employee1);
+        return ResponseEntity.ok(updatedEmployee);
+
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<Map<String,Boolean>>deleteEmployee(@PathVariable Long id){
+        Employee employee= employeeRepositorie.findById(id)
+                .orElseThrow(() -> new RuntimeException("No existe el empleado con el ID : " + id));
+
+        employeeRepositorie.delete(employee);
+        Map<String, Boolean> res = new HashMap<>();
+        res.put("eliminar",Boolean.TRUE);
+        return ResponseEntity.ok(res);
     }
 }
